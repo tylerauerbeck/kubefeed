7ac65a3b924c196391e823babaf0c1a02248d8fb
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2025-12-20 03:17:40 -0500

    add scalar openapi ui
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

---
+++ pages/docs/openapi.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ApiReference : configuration="config" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ApiReference } from '@scalar/api-reference'
+import '@scalar/api-reference/style.css'
+
+const config = {
+  spec: {
+    url: '/openapi.json'
+  },
+  theme: 'default', // or 'alternate', 'moon', 'purple', 'solarized'
+}
+</script>
