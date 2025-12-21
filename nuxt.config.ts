7c66752a3df5762756009eb7f46e4438ca8c236a
Tyler Auerbeck <29497147+tylerauerbeck@users.noreply.github.com>
2025-12-21 18:53:25 -0500

    Update icon (#11)
    * update icon config
    
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
    
    * cleanup icons
    
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
    Co-authored-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  app: {
-    head: {
-      link: [
-        { rel: 'icon', type: 'image/png', href: '/kubefeed.png' }
-      ]
-    }
-  }
+  css: ['~/assets/css/main.css']
 })
